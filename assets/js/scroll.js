82c5e4039da13767b6c4c71f7ebd6def6d21cd8f
davalerova <dvdovni@gmail.com>
2021-08-14 16:04:55 -0500

    Se crea la sección footer

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,1 +1,15 @@
-alert("Si funciona");
+const header = document.getElementById("header");
+
+document.addEventListener("scroll", scrollHeaderTop);
+
+function scrollHeaderTop(){
+
+    if(window.scrollY > 500){
+        console.log(window.scrollY);
+        header.classList.remove("header");
+        header.classList.add("fixed-header");
+    }else if(window.scrollY === 0){
+        header.classList.remove("fixed-header");
+    }
+}
+
